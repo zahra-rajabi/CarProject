cdf8ca2a316fc991b0f3717efb108e7886048f9f
Zahra Rajabi <zahra.80.rajabi@gmail.com>
2024-08-19 02:33:45 +0330

    category list created

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,8 +1,29 @@
+import Suv from "./icons/Suv";
+import Sedan from "./icons/Sedan";
+import Sport from "./icons/Sport";
+import Hatchback from "./icons/Hatchback";
 
 function Categories() {
   return (
-    <div>Categories</div>
-  )
+    <ul className="flex items-center gap-4">
+      <li className="flex items-center justify-between">
+        <Suv />
+        <p>Suv</p>
+      </li>
+      <li className="flex items-center justify-between">
+        <Sedan />
+        <p>Sedan</p>
+      </li>
+      <li className="flex items-center justify-between">
+        <Sport />
+        <p>Sport</p>
+      </li>
+      <li className="flex items-center justify-between">
+        <Hatchback />
+        <p>Hatchback</p>
+      </li>
+    </ul>
+  );
 }
 
-export default Categories
+export default Categories;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Categories from "@/components/Categories";
 import Range from "@/components/Range";
 
 export default function Home() {
   return (
     <>
       <Range />
+      <Categories />
     </>
   );
 }
